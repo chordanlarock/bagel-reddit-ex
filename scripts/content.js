e71fe6ceafe7ba8f5777cc43ed6f8d356429ac71
chordanlarock <chordanlarock@gmail.com>
2022-10-15 11:11:55 -0400

    Add mutation observable
    The header gets dynamically created at times, so now we insert the r/all button on page load, and create an observable if the r/all button does not exist for future mutations (the observable triggers the same btn-adding).

--- scripts/content.js
+++ scripts/content.js
@@ -1,19 +1,56 @@
-const popularBtn = document.querySelector("[href='/r/popular/']");
-const popularBtnContainer = popularBtn.parentElement;
+// const selector = "#SearchDropdown";
+const selector = "[href='/r/popular/']";
 
-if (popularBtnContainer) {
-const badge = document.createElement("a");
-  badge.href="/r/all/"
+let count = 0;
+const containerId = 'bagel-container'
 
-  badge.classList.add(popularBtn.className, "type--icon");
-  badge.style.display = 'flex';
-  badge.style.alignItems = 'center';
-  badge.textContent = `/ALL`;
+const header = document.querySelector('header');
 
-  const myContainer = document.createElement("div");
-  // myContainer.classList.add(popularBtnContainer.className, "type--div");
-  myContainer.style.marginLeft = '8px';
-  myContainer.appendChild(badge);
+function insert() {
+  const popularBtn = document.querySelector(selector);
+  const popularBtnContainer = popularBtn?.parentElement;
+  if (popularBtnContainer) {
+    const badge = document.createElement("a");
+    badge.href = "/r/all/"
 
-  (popularBtnContainer).insertAdjacentElement("afterend", myContainer);
+    badge.className = popularBtn.className;
+    badge.style.display = 'flex';
+    badge.style.alignItems = 'center';
+    badge.textContent = `/ALL`;
+
+    const myContainer = document.createElement("div");
+    // myContainer.classList.add(popularBtnContainer.className, "type--div");
+    myContainer.style.marginLeft = '8px';
+    myContainer.appendChild(badge);
+    myContainer.id = containerId;
+
+    (popularBtnContainer).insertAdjacentElement("afterend", myContainer);
+  }
 }
+
+insert();
+
+if (header) {
+  const observer = new MutationObserver(_mutations => {
+    try {
+      if (document.querySelector(selector) && !document.querySelector("[href='/r/all/']")) {
+        console.log('found popular button');
+        insert();
+        count++;
+        if (document.querySelectorAll('#' + containerId).length || count > 50) {
+          console.log('dc');
+          observer.disconnect();
+        }
+      }
+    } catch (e) {
+      console.warn(e);
+      observer.disconnect();
+    }
+  });
+
+  observer.observe(document.querySelector('header'), {
+    childList: true,
+    subtree: true,
+  });
+}
+
